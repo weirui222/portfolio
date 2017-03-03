160ce19928fadc1f3db2b37e3aced742c1de8ebf
weirui222 <congcongli@live.com>
2017-03-02 20:21:41 -0800

    Responsive project table

--- js/scrollDirective.js
+++ js/scrollDirective.js
@@ -1,17 +1,20 @@
 angular.module('scrollDirective', [])
 // Nav scroller directive
 .directive("scroll", function ($window) {
-    return function(scope, element, attrs) {
-        angular.element($window).bind("scroll", function() { //bind scroll directive to window
-          var navi = angular.element(document.getElementsByClassName("navbar-fixed-top"));
-             if (this.pageYOffset >= 620) {
-                 scope.boolChangeClass = true;
-                 navi.removeClass('minfade').addClass('navSpy'); //only fade in navbar bg if user has scrolled past 620px once
-             } else if (navi.hasClass('navSpy')){
-                 scope.boolChangeClass = false;
-                 navi.addClass('minfade');
-             }
-            scope.$apply();
-        });
-    };
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() { //bind scroll directive to window
+      var navi = angular.element(document.getElementsByClassName("navbar-fixed-top"));
+      var slogan = $("#slogan");
+      if (this.pageYOffset >= 620) {
+         scope.boolChangeClass = true;
+         navi.removeClass('minfade').addClass('navSpy'); //only fade in navbar bg if user has scrolled past 620px once
+         slogan.hide();
+      } else if (navi.hasClass('navSpy')){
+         scope.boolChangeClass = false;
+         navi.addClass('minfade');
+         slogan.show();
+      }
+      scope.$apply();
+    });
+  };
 });
